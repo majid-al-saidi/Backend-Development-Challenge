74b1764bc7ed926d8017747dab523d66f9817001
majid-al-saidi <majid-al-saidi@outlook.com>
2022-05-01 01:32:07 +0400

    desgin resume update page

--- resources/views/admin/resume/edit.blade.php
+++ resources/views/admin/resume/edit.blade.php
@@ -1,21 +1,18 @@
-@extends('layouts.admin')
-@section('content')
-<div class="row">
-    <div class="card bg-blueGray-100">
-        <div class="card-header">
-            <div class="card-header-container">
-                <h6 class="card-title">
-                    {{ trans('global.edit') }}
-                    {{ trans('cruds.resume.title_singular') }}:
-                    {{ trans('cruds.resume.fields.id') }}
-                    {{ $resume->id }}
-                </h6>
-            </div>
-        </div>
 
-        <div class="card-body">
-            @livewire('resume.edit', [$resume])
-        </div>
+@extends('layouts.app')
+
+@section('content')
+ 
+<!-- component -->
+<div class=" w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="w-full sm:max-w-md p-5 mx-auto">
+      <h2 class="mb-12 text-center text-4xl font-extrabold uppercase ">Edit and Update Resume
+          {{-- <center>
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_lrw0segg.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay></lottie-player>
+          </center> --}}
+      </h2>
+      @livewire('resume.edit', [$resume])
     </div>
-</div>
+  </div>
 @endsection
--- resources/views/livewire/resume/edit.blade.php
+++ resources/views/livewire/resume/edit.blade.php
@@ -2,7 +2,7 @@
 
     <div class="form-group {{ $errors->has('resume.title') ? 'invalid' : '' }}">
         <label class="form-label required" for="title">{{ trans('cruds.resume.fields.title') }}</label>
-        <input class="form-control" type="text" name="title" id="title" required wire:model.defer="resume.title">
+        <input class="form-controle mt-1 block w-full border-none bg-gray-100 h-11 rounded-full text-center shadow-sm hover:bg-blue-100 focus:bg-blue-100 focus:ring-0" type="text" name="title" id="title" required wire:model.defer="resume.title">
         <div class="validation-message">
             {{ $errors->first('resume.title') }}
         </div>
@@ -12,7 +12,8 @@
     </div>
     <div class="form-group {{ $errors->has('mediaCollections.resume_resume') ? 'invalid' : '' }}">
         <label class="form-label required" for="resume">{{ trans('cruds.resume.fields.resume') }}</label>
-        <x-dropzone id="resume" name="resume" action="{{ route('admin.resumes.storeMedia') }}" collection-name="resume_resume" max-file-size="5" max-files="1" />
+        <x-dropzone id="resume" name="resume" style="border: none; --tw-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+        box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow); background-color: rgb(243 244 246);" action="{{ route('admin.resumes.storeMedia') }}" collection-name="resume_resume" max-file-size="5" max-files="1" />
         <div class="validation-message">
             {{ $errors->first('mediaCollections.resume_resume') }}
         </div>
@@ -22,7 +23,7 @@
     </div>
     <div class="form-group {{ $errors->has('resume.status') ? 'invalid' : '' }}">
         <label class="form-label">{{ trans('cruds.resume.fields.status') }}</label>
-        <select class="form-control" wire:model="resume.status">
+        <select class="w-full h-11 border-none bg-gray-100 rounded-full text-center shadow-sm hover:bg-blue-100" wire:model="resume.status">
             <option value="null" disabled>{{ trans('global.pleaseSelect') }}...</option>
             @foreach($this->listsForFields['status'] as $key => $value)
                 <option value="{{ $key }}">{{ $value }}</option>
@@ -46,12 +47,16 @@
         </div>
     </div>
 
-    <div class="form-group">
-        <button class="btn btn-indigo mr-2" type="submit">
+    <div class="flex form-group">
+        <button class="bg-green-500 mx-5 w-1/2 py-3 rounded-full text-white shadow-sm hover:shadow-inner  transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-95" type="submit">
             {{ trans('global.save') }}
         </button>
-        <a href="{{ route('admin.resumes.index') }}" class="btn btn-secondary">
-            {{ trans('global.cancel') }}
+        <a href="{{ route('admin.resumes.index') }}" class="btn mx-5 justify-self-center bg-red-50 w-1/2 py-3 rounded-full text-black shadow-sm hover:shadow-inner  transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-95">
+           <p style="margin: 0;
+           position: absolute;
+           top: 50%;
+           left: 50%;
+           transform: translate(-50%, -50%);"> {{ trans('global.cancel') }} </p>
         </a>
     </div>
 </form>
