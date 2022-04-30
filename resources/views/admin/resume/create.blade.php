c7297cfe48b3a8d8db8ade145cf1791a534b8a95
majid-al-saidi <majid-al-saidi@outlook.com>
2022-05-01 01:23:08 +0400

    update desgin of resume creation

--- resources/views/admin/resume/create.blade.php
+++ resources/views/admin/resume/create.blade.php
@@ -1,19 +1,17 @@
-@extends('layouts.admin')
-@section('content')
-<div class="row">
-    <div class="card bg-blueGray-100">
-        <div class="card-header">
-            <div class="card-header-container">
-                <h6 class="card-title">
-                    {{ trans('global.create') }}
-                    {{ trans('cruds.resume.title_singular') }}
-                </h6>
-            </div>
-        </div>
+@extends('layouts.app')
 
-        <div class="card-body">
-            @livewire('resume.create')
-        </div>
+@section('content')
+ 
+<!-- component -->
+<div class=" w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+    <div class="w-full sm:max-w-md p-5 mx-auto">
+      <h2 class="mb-12 text-center text-4xl font-extrabold uppercase ">Upload New Resume
+          <center>
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_lrw0segg.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"  loop  autoplay></lottie-player>
+          </center>
+      </h2>
+      @livewire('resume.create')
     </div>
-</div>
+  </div>
 @endsection
